8538dd7fec35329311aaa3fb10ff2a00ef8a93bc
NisimAtiya <118813292+NisimAtiya@users.noreply.github.com>
2023-01-11 10:59:50 +0200

    Update Task.java
    Added Javadoc to task

--- Ex2/src/B/Task.java
+++ Ex2/src/B/Task.java
@@ -2,26 +2,48 @@ package B;
 
 import java.util.concurrent.Callable;
 
+/**
+ * Represents an operation that can be run asynchronously and can return a value of some type with some priority
+ */
 public class Task<T> implements Comparable<Task<T>>, Callable<T> {
     private TaskType taskType;
     private Callable<T> callable;
-    //A regular constructor
+
+    /**
+     * A private constructor that accepts a Callable and a TaskType
+     */
     private Task(Callable<T> callable1,TaskType taskType1){
         setTaskType(taskType1);
         setCallable( callable1);
     }
-    //constructor without B.TaskType
+    /**
+     * A private constructor that accepts a Callable and puts TaskType OTHER
+     */
     private Task(Callable<T> callable1){
         setCallable(callable1);
         setTaskType(TaskType.OTHER);
     }
+
+    /**
+     * Factory method
+     * @return Task
+     */
     public static <T> Task createTask(Callable callable1, TaskType taskType1){
         return new Task(callable1,taskType1);
     }
+    /**
+     * Factory method
+     * @return Task
+     */
     public static <T> Task createTask(Callable callable1){
         return new Task(callable1);
     }
 
+    /**
+     * Compares the taskType
+     * @param o the object to be compared.
+     * @return 1 if I am better, -1 if he is better, 0 if we are equal
+     */
     @Override
     public int compareTo(Task<T> o) {
         if(this.getTaskType().getPriorityValue()<o.getTaskType().getPriorityValue()){
@@ -33,6 +55,10 @@ public class Task<T> implements Comparable<Task<T>>, Callable<T> {
         }
     }
 
+    /**
+     * Activates the call function
+     * @throws Exception
+     */
     @Override
     public T call() throws Exception {
         try {
@@ -43,22 +69,40 @@ public class Task<T> implements Comparable<Task<T>>, Callable<T> {
         }
     }
 
+    /**
+     * function getTaskType
+     * @return the TaskType
+     */
     public TaskType getTaskType() {
         return taskType;
     }
 
+    /**
+     * function setTaskType
+     * @param taskType to set
+     */
     public void setTaskType(TaskType taskType) {
         this.taskType = taskType;
     }
-
+    /**
+     * function getCallable
+     * @return the Callable
+     */
     public  Callable<T> getCallable() {
         return callable;
     }
-
+    /**
+     * function setCallable
+     * @param callable to set
+     */
     public void setCallable(Callable<T> callable) {
         this.callable = callable;
     }
 
+    /**
+     * toString function
+     * @return A string representing the task
+     */
     @Override
     public String toString() {
         return "B.Task{" +
@@ -66,22 +110,4 @@ public class Task<T> implements Comparable<Task<T>>, Callable<T> {
                 ", callable=" + getCallable() +
                 '}';
     }
-public static void main(String[] args) {
-        var task = Task.createTask(()->{
-            int sum = 0;
-            for (int i = 1; i <= 10; i++) {
-                sum += i; }
-            return sum;
-        }, TaskType.IO);
-        var task2 = Task.createTask(()->{
-            int sum = 0;
-            for (int i = 1; i <= 10; i++) {
-                sum += i; }
-            return sum;
-        }, TaskType.COMPUTATIONAL);
-    System.out.println(task.toString());
-    }
-
-
-
 }
